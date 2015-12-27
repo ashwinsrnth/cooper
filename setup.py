9763aba27c791a6d5aa3491d5918c6406cb102ef
Ashwin Srinath <ashwinsrnth@gmail.com>
2015-12-26 19:03:32 -0500

    put things in setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+ setup(name='cooper',
+  version='1.0',
+  description='Naive array permutations with CUDA',
+  author='Ashwin Srinath',
+  author_email='atrikut@g.cleemson.edu',
+  url='http://github.com/ashwinsrnth/cooper',
+  packages=['distutils', 'distutils.command'],
+ )
+
+
