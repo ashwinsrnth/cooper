784abd1c13e601808a02f88cd53f7532076f7baf
Ashwin Srinath <ashwinsrnth@gmail.com>
2015-12-26 19:16:23 -0500

    update benchmarks'

--- bench/benchmarks/benchmarks.py
+++ bench/benchmarks/benchmarks.py
@@ -1,37 +1,17 @@
 # Write the benchmarking functions here.
 # See "Writing benchmarks" in the asv docs for more information.
 import numpy as np
+from pycuda import autoinit
 import pycuda.driver as cuda
+import pycuda.gpuarray as gpuarray
+from permute import permute
+import itertools
 
-class TimeSuite:
-    """
-    An example benchmark that times the performance of various kinds
-    of iterating over dictionaries in Python.
-    """
-    def setup(self):
-        self.d = {}
-        for x in range(500):
-            self.d[x] = None
+def time_permute():
+    a = np.random.rand(32, 32, 32)
+    b = np.zeros((32, 32, 32), dtype=np.float64)
+    a_d = gpuarray.to_gpu(a)
+    b_d = gpuarray.to_gpu(b)
+    permute(a_d, b_d, permutation)
+    assert_allclose(b_d.get(), a.transpose(permutation).copy())
 
-    def time_keys(self):
-        for key in self.d.keys():
-            pass
-
-    def time_iterkeys(self):
-        for key in self.d.iterkeys():
-            pass
-
-    def time_range(self):
-        d = self.d
-        for key in range(500):
-            x = d[key]
-
-    def time_xrange(self):
-        d = self.d
-        for key in xrange(500):
-            x = d[key]
-
-
-class MemSuite:
-    def mem_list(self):
-        return [0] * 256
